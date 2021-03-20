3c980fbfeed6421df1174a68e03771e6f3363847
Parimal Dave <parimal.s.dave@gmail.com>
2021-03-20 19:16:52 +0530

    testing husky 1

--- testapp/src/App.js
+++ testapp/src/App.js
@@ -4,14 +4,14 @@ import "./App.css";
 function App() {
 var test = "test"
  return (
- <div className="App">
-  <header className="App-header">
-  <img src={logo} className="App-logo" alt="logo" />
-  <p>
-   Edit <code>src/App.js</code> and save to reload {test}.
- </p>
-        <a
-   className="App-link"
+<div className="App">
+<header className="App-header">
+<img src={logo} className="App-logo" alt="logo" />
+<p>
+Edit <code>src/App.js</code> and save to reload {test}.
+</p>
+<a
+className="App-link"
           href="https://reactjs.org"
           target="_blank"
     rel="noopener noreferrer"
