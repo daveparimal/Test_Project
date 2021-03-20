51e5295c1bcef37a4e323689aa727968d8b1ca46
Parimal Dave <parimal.s.dave@gmail.com>
2021-03-20 19:20:19 +0530

    testing husky 2

--- testapp/src/App.js
+++ testapp/src/App.js
@@ -12,11 +12,11 @@ Edit <code>src/App.js</code> and save to reload {test}.
 </p>
 <a
 className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-    rel="noopener noreferrer"
-   >
-          Learn React
+     href="https://reactjs.org"
+       target="_blank"
+ rel="noopener noreferrer"
+ >
+    Learn React
         </a>
    </header>
   </div>
